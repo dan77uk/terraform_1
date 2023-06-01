50595f55eaef398098df57acda03296209b21f2f
Dan Phillips <89780130+dan77uk@users.noreply.github.com>
2023-06-01 23:25:37 +0100

    count and conditionals added

--- main.tf
+++ main.tf
@@ -13,23 +13,37 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_instance" "first_instance" {
+resource "aws_instance" "dev_instance" {
   ami = "ami-0e1c5be2aa956338b"
-  instance_type = var.instance_type_size
+  # instance_type = var.instance_map["eu1m"]
+  # instance_type = var.instance_list[1]
+  instance_type = "t2.micro"
+  # count = 3
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
-    Name = "terraform_1"
+    Name = "Test Instance"
+  }
+  count = var.is_test_env == "no" ? 1 : 0
+}
+
+resource "aws_instance" "prod_instance" {
+  ami = "ami-0e1c5be2aa956338b"
+  instance_type = "t2.large"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  count = var.is_test_env == "yes" ? 3 : 0
+   tags = {
+    Name = "Prod Instance"
   }
 }
 
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.first_instance.id
-  allocation_id = aws_eip.terraform_1.id
-}
+# resource "aws_eip_association" "eip_assoc" {
+#   instance_id   = aws_instance.first_instance.id
+#   allocation_id = aws_eip.terraform_1.id
+# }
 
-resource "aws_eip" "terraform_1" {
-  domain   = "vpc"
-}
+# resource "aws_eip" "terraform_1" {
+#   domain   = "vpc"
+# }
 
 resource "aws_security_group" "allow_tls" {
   name = "allow_tls"
@@ -60,7 +74,7 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-output "first_instance_ip" {
-  value = aws_instance.first_instance.public_ip
-}
+# output "first_instance_ip" {
+#   value = aws_instance.first_instance.public_ip
+# }
 
--- variables.tf
+++ variables.tf
@@ -2,3 +2,30 @@ variable "security_group_vpn_ip" {}
 variable "instance_type_size" {}
 variable "access_key" {}
 variable "secret_key" {}
+# variable "instance_tags" {
+#   description = "Project Name and Build Version"
+#   type = number
+# }
+
+# Lists and Maps
+# variable "instance_map" {
+#   type = map
+#   default = {
+#     eu1m = "t1.micro"
+#     eu2m = "t2.micro"
+#   }
+# }
+variable "instance_list" {
+  type = list
+  default = ["t1.micro","t2.micro"]
+}
+
+variable "name_list" {
+  type = list
+  default = ["dev_instance", "test_instance", "prod_instance"]
+}
+
+variable "is_test_env" {
+  type = string
+  description = "Is this a production environment (yes/no)?"
+}
