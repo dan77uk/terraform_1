71807255d393e808a209132eba8f956a0c14fff0
Dan Phillips <89780130+dan77uk@users.noreply.github.com>
2023-06-07 13:36:49 +0100

    test run

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "first_instance_ip" {
 #   value = [for name in var.names : upper(name)]
 # }
 
-output "vault_secrets" {
-  value     = data.vault_generic_secret.demo.data_json
-  sensitive = true
-}
+# output "vault_secrets" {
+#   value     = data.vault_generic_secret.demo.data_json
+#   sensitive = true
+# }
